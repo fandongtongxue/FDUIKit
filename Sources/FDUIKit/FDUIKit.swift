ad299e09961e5ed5e256fd4ed0d484a52879ca9a
fandong <admin@fandong.me>
2020-02-23 20:46:08 +0800

    Version

--- Sources/FDUIKit/FDUIKit.swift
+++ Sources/FDUIKit/FDUIKit.swift
@@ -1,1 +1,13 @@
+import Foundation
 
+public class FDUIKit : NSObject{
+
+//class func
+public class func getVersion() -> String {
+    return "1.0"
+}
+
+public class func getBuildVersion() -> String {
+    return "20200223"
+}
+}
