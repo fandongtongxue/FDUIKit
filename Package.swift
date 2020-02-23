354d5eb6a3f754c25f4ec45330cd2c4d2d422d2e
fandong <admin@fandong.me>
2020-02-24 07:33:11 +0800

    Kingfisher

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.0.0"),
         .package(url: "https://github.com/airbnb/lottie-ios.git", from: "3.1.2"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,7 +27,7 @@ let package = Package(
         .target(
             name: "FDUIKit",
             dependencies: [
-            "SnapKit","Lottie",
+            "SnapKit","Lottie","Kingfisher",
         ]),
         .testTarget(
             name: "FDUIKitTests",
