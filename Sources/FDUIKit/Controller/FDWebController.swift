a3f6af29db4b6140d96e5cbcc99551f2eaede7df
fandong <admin@fandong.me>
2020-02-26 19:08:57 +0800

    fix

--- Sources/FDUIKit/Controller/FDWebController.swift
+++ Sources/FDUIKit/Controller/FDWebController.swift
@@ -13,12 +13,14 @@ public class FDWebController : FDBaseController {
     
     public let url : URL
     
+    init(url : URL) {
+        self.url = url;
+    }
+    
     public override func viewDidLoad() {
         let config = WKWebViewConfiguration.init()
         let webView = WKWebView.init(frame: view.bounds, configuration: config)
         webView.load(URLRequest.init(url: url))
-        webView.navigationDelegate = self
-        webView.uiDelegate = self
         view.addSubview(webView)
     }
 }
