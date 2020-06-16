867baaa54382efde7dbc3cd2c63ae496701ced5a
fandong <admin@fandong.me>
2020-06-16 15:44:00 +0800

    update

--- Sources/FDUIKit/Define/FDDefine.swift
+++ Sources/FDUIKit/Define/FDDefine.swift
@@ -10,7 +10,7 @@ import UIKit
 
 public let FD_ScreenWidth = UIScreen.main.bounds.size.width;
 public let FD_ScreenHeight = UIScreen.main.bounds.size.height;
-public let FD_NavigationHeight = 44.0f;
+public let FD_NavigationHeight = 44.0;
 public let FD_StatusBarHeight = UIApplication.shared.statusBarFrame.size.height;
 public let FD_TopHeight = FD_StatusBarHeight + FD_NavigationHeight;
 public let FD_TabBarHeight = 49 + (FD_StatusBarHeight > 20 ? 34 : 0);
