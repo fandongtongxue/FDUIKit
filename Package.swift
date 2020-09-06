6e93048384d3685f2e3fc3f7f83e8fb26671dbef
fandongtongxue <admin@fandong.me>
2020-09-06 11:11:32 +0800

    update

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.0.0"),
-        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.0.0"),
+//        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.0.0"),
+//        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,7 +23,7 @@ let package = Package(
         .target(
             name: "FDUIKit",
             dependencies: [
-            "SnapKit","Kingfisher",
+//            "SnapKit","Kingfisher",
         ]),
         .testTarget(
             name: "FDUIKitTests",
