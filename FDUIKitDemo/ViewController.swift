ccf7a06bd8fcb0017dfa273f13fcd3cc9d1ab885
fandongtongxue <admin@fandong.me>
2020-09-06 11:24:39 +0800

    fix

--- FDUIKitDemo/ViewController.swift
+++ FDUIKitDemo/ViewController.swift
@@ -19,6 +19,11 @@ class ViewController: UIViewController {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
+        //FDPopView
+//        let popView = FDPopView.init(frame: CGRect.init(x: 0, y: FD_ScreenHeight, width: FD_ScreenWidth, height: 300))
+//        popView.backgroundColor = .cyan
+//        popView.show(view, .bottom)
+        //FDWebController
         let webVC = FDWebController()
         webVC.url = "http://baidu.com"
         present(webVC, animated: true, completion: nil)
